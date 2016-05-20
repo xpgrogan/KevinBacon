168c7d7233cdd19f692aed8ae610a127f0f5e5af
Xavier <xavier.grogan@aol.com>
2016-05-20 18:55:17 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+//Xavier Grogan
+//4-20-16
+//Programming Assignment 4
+//In keeping with School of Engineering honor code, I hereby state, that this is my own work
+//and I did not plagiarize.
+//
+//Compiled with Eclipse
+//
+
+public class Main 
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Bacon bacon = new Bacon();
+		bacon.bacon();
+	}
+}
